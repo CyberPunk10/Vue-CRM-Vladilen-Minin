66ea7ddeabdd37c2fc74f608c1f8f89cd2387e4a
CyberPunk10 <konstantin21_arx@gmail.com>
2020-08-08 15:32:17 +0300

    Lesson 1-3 (Vladilen Minin)

---
+++ src/layouts/EmptyBlueLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=bgred>
+    <div class="app-page">
+
+        <router-view />
+
+      </div>
+  </div>
+</template>
+
+<style lang="sass">
+.bgred
+  width: 100%
+  height: 100%
+  min-height: 100vh
+  background-color: #dfefef
+
+</style>
